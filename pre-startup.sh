5ecd1900bc9c47bcb42e5fa1598792aa2f8c92a0
NigelThomas <nigel.cl.thomas@gmail.com>
2020-01-13 14:43:36 +0000

    ensure SQLSTREAM_HOME set for pre-startup

--- pre-startup.sh
+++ pre-startup.sh
@@ -6,13 +6,16 @@
 
 # we depend on xmllint to parse the data
 
+. /etc/sqlstream/environment
+export SQLSTREAM_HOME
+
 apt-get update
 apt-get install -y ant libxml2-utils
 
 mkdir -p ./lib
 ./getLocalLibs.sh
 ant
-chown -R sqlstream:sqlstream classes libs Polygon.jar
+chown -R sqlstream:sqlstream classes lib Polygon.jar
 
 $SQLSTREAM_HOME/bin/sqllineClient --run=polygonContains.sql
 
