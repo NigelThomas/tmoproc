1500b7a922b02fea85957e53cd822ee3ee12473a
NigelThomas <nigel.cl.thomas@gmail.com>
2020-01-13 14:36:58 +0000

    test pre-startup.sj

--- pre-startup.sh
+++ pre-startup.sh
@@ -7,12 +7,14 @@
 # we depend on xmllint to parse the data
 
 apt-get update
-apt-get install -y libxml2-utils
+apt-get install -y ant libxml2-utils
 
+mkdir -p ./lib
+./getLocalLibs.sh
+ant
+chown -R sqlstream:sqlstream classes libs Polygon.jar
 
-su sqlstream -m -c ./getLocalLibs.sh
-su sqlstream -m -c ant
-$SQLSTREAM_HOME/bin/sqllineClient --run polygonContains.sql
+$SQLSTREAM_HOME/bin/sqllineClient --run=polygonContains.sql
 
 echo ... Downloading project data in the background
 
